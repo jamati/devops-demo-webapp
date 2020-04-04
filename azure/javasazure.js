3b711326551e205c5fc7c41fbf98676cbb90993a
Fabiano Jamati <fajamati@microsoft.com>
2020-04-04 19:21:28 -0300

    Primeiro commit

---
+++ azure/javasazure.js
@@ -0,0 +1,175 @@
+var content0 = document.getElementById("model0");
+var button0 = document.getElementById("show-more0");
+
+var content1 = document.getElementById("model1");
+var button1 = document.getElementById("show-more1");
+
+var content2 = document.getElementById("model2");
+var button2 = document.getElementById("show-more2");
+
+var content3 = document.getElementById("model3");
+var button3 = document.getElementById("show-more3");
+
+var content4 = document.getElementById("model4");
+var button4 = document.getElementById("show-more4");
+
+var content5 = document.getElementById("model5");
+var button5 = document.getElementById("show-more5");
+
+var content6 = document.getElementById("model6");
+var button6 = document.getElementById("show-more6");
+
+var content7 = document.getElementById("model7");
+var button7 = document.getElementById("show-more7");
+
+var content8 = document.getElementById("model8");
+var button8 = document.getElementById("show-more8");
+
+var content9 = document.getElementById("model9");
+var button9 = document.getElementById("show-more9");
+
+var content10 = document.getElementById("model10");
+var button10 = document.getElementById("show-more10");
+
+button0.onclick = function () {
+
+    if (content0.className == "open") {
+        content0.className = "";
+        button0.innerHTML = "+info";
+
+    } else {
+        content0.className = "open";
+        button0.innerHTML = "-info";
+    }
+
+}
+
+button1.onclick = function () {
+
+    if (content1.className == "open") {
+        content1.className = "";
+        button1.innerHTML = "+info";
+
+    } else {
+        content1.className = "open";
+        button1.innerHTML = "-info";
+    }
+
+}
+
+button2.onclick = function () {
+
+    if (content2.className == "open") {
+        content2.className = "";
+        button2.innerHTML = "+info";
+
+    } else {
+        content2.className = "open";
+        button2.innerHTML = "-info";
+    }
+
+}
+
+button3.onclick = function () {
+
+    if (content3.className == "open") {
+        content3.className = "";
+        button3.innerHTML = "+info";
+
+    } else {
+        content3.className = "open";
+        button3.innerHTML = "-info";
+    }
+
+}
+
+button4.onclick = function () {
+
+    if (content4.className == "open") {
+        content4.className = "";
+        button4.innerHTML = "+info";
+
+    } else {
+        content4.className = "open";
+        button4.innerHTML = "-info";
+    }
+
+}
+
+button5.onclick = function () {
+
+    if (content5.className == "open") {
+        content5.className = "";
+        button5.innerHTML = "+info";
+
+    } else {
+        content5.className = "open";
+        button5.innerHTML = "-info";
+    }
+
+}
+
+button6.onclick = function () {
+
+    if (content6.className == "open") {
+        content6.className = "";
+        button6.innerHTML = "+info";
+
+    } else {
+        content6.className = "open";
+        button6.innerHTML = "-info";
+    }
+
+}
+
+button7.onclick = function () {
+
+    if (content7.className == "open") {
+        content7.className = "";
+        button7.innerHTML = "+info";
+
+    } else {
+        content7.className = "open";
+        button7.innerHTML = "-info";
+    }
+
+}
+
+button8.onclick = function () {
+
+    if (content8.className == "open") {
+        content8.className = "";
+        button8.innerHTML = "+info";
+
+    } else {
+        content8.className = "open";
+        button8.innerHTML = "-info";
+    }
+
+}
+
+button9.onclick = function () {
+
+    if (content9.className == "open") {
+        content9.className = "";
+        button9.innerHTML = "+info";
+
+    } else {
+        content9.className = "open";
+        button9.innerHTML = "-info";
+    }
+
+}
+
+button10.onclick = function () {
+
+    if (content10.className == "open") {
+        content10.className = "";
+        button10.innerHTML = "+info";
+
+    } else {
+        content10.className = "open";
+        button10.innerHTML = "-info";
+    }
+
+}
---
+++ dynamics365/javasdynamics.js
@@ -0,0 +1,117 @@
+var content0 = document.getElementById("model0");
+var button0 = document.getElementById("show-more0");
+
+var content1 = document.getElementById("model1");
+var button1 = document.getElementById("show-more1");
+
+var content2 = document.getElementById("model2");
+var button2 = document.getElementById("show-more2");
+
+var content3 = document.getElementById("model3");
+var button3 = document.getElementById("show-more3");
+
+var content4 = document.getElementById("model4");
+var button4 = document.getElementById("show-more4");
+
+
+var content5 = document.getElementById("model5");
+var button5 = document.getElementById("show-more5");
+
+
+var content6 = document.getElementById("model6");
+var button6 = document.getElementById("show-more6");
+
+button0.onclick = function () {
+
+    if (content0.className == "open") {
+        content0.className = "";
+        button0.innerHTML = "+info";
+
+    } else {
+        content0.className = "open";
+        button0.innerHTML = "-info";
+    }
+
+}
+
+button1.onclick = function () {
+
+    if (content1.className == "open") {
+        content1.className = "";
+        button1.innerHTML = "+info";
+
+    } else {
+        content1.className = "open";
+        button1.innerHTML = "-info";
+    }
+
+}
+
+button2.onclick = function () {
+
+    if (content2.className == "open") {
+        content2.className = "";
+        button2.innerHTML = "+info";
+
+    } else {
+        content2.className = "open";
+        button2.innerHTML = "-info";
+    }
+
+}
+
+button3.onclick = function () {
+
+    if (content3.className == "open") {
+        content3.className = "";
+        button3.innerHTML = "+info";
+
+    } else {
+        content3.className = "open";
+        button3.innerHTML = "-info";
+    }
+
+}
+
+button4.onclick = function () {
+
+    if (content4.className == "open") {
+        content4.className = "";
+        button4.innerHTML = "+info";
+
+    } else {
+        content4.className = "open";
+        button4.innerHTML = "-info";
+    }
+
+}
+
+
+
+
+button5.onclick = function () {
+
+    if (content5.className == "open") {
+        content5.className = "";
+        button5.innerHTML = "+info";
+
+    } else {
+        content5.className = "open";
+        button5.innerHTML = "-info";
+    }
+
+}
+
+
+button6.onclick = function () {
+
+    if (content6.className == "open") {
+        content6.className = "";
+        button6.innerHTML = "+info";
+
+    } else {
+        content6.className = "open";
+        button6.innerHTML = "-info";
+    }
+
+}
